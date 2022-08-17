32b90f5317a470fef45520c4eb8acdacb01c7e25
forgacspeter94 <forgacspeter940@gmail.com>
2022-08-17 16:53:03 +0200

    adding css and changes

--- frontend/script.js
+++ frontend/script.js
@@ -4,10 +4,16 @@ function textComponent (){
     return `
     <h1> Visszajelzés </h1>
     <input type="text" class="text" placeholder="Tárgy">
+    <div class="input">
     <textarea cols="30" rows="10" placeholder="Megjegyzés"></textarea>
-    <input type="checkbox"class = 'checkbox' name="Adatkezelés">
-    <label for="AdataKezelés">Elolvastam és elfogadom az  <a href="#" class= "data-link">Adatkezelési Tájékoztatót</a>.</div></label>
-    <button>MENTÉS</button>
+    </div>
+    <div class="checkboxflex">
+        <input type="checkbox" class = 'checkbox' name="Adatkezelés">
+        <label for="AdataKezelés">Elolvastam és elfogadom az  <a href="#" class= "data-link">Adatkezelési Tájékoztatót</a>.</div></label>
+    </div>
+    <div class ="button-container">
+        <button>MENTÉS</button>
+    </div>
     `
 }
 
